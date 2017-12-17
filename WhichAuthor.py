ea0ecad7ce6075ef7947ce1f9c2ab703832ab35c
Duncan Sollers <duncan.a.sollers@gmail.com>
2017-12-16 23:12:42 -0500

    Stop Words Working

--- WhichAuthor.py
+++ WhichAuthor.py
@@ -4,9 +4,7 @@ print ("Hello World")
 print ("Hello World")
 print ("Hello World")
 
-#1.
-#Open & Read the contents of the smallwords file into a dictionary and print it out
-#Close the file
+
 
 #2.
 #Open and Read one of the Authors files into a dictionary.
@@ -17,3 +15,22 @@ print ("Hello World")
 #
 #3 print out this dictionary
 #
+
+
+
+stopwords = {}
+with open("smallwords/StopWords.txt") as f:
+    for line in f:
+        line2 = line.replace('\n','')
+        stopwords[line2] = line2
+
+
+for k in stopwords.items():
+    print(k)
+
+authorwords = {}
+with open("smallwords/StopWords.txt") as f:
+    for line in f:
+        line2 = line.replace('\n','')
+        authorwords[line2] = line2
+
