c6a66ef361ed57446425259165bf9b0cab2634b5
William Sollers <wsollers@gmail.com>
2017-12-16 22:37:59 -0500

    Instructions

--- WhichAuthor.py
+++ WhichAuthor.py
@@ -3,3 +3,17 @@
 print ("Hello World")
 print ("Hello World")
 print ("Hello World")
+
+#1.
+#Open & Read the contents of the smallwords file into a dictionary and print it out
+#Close the file
+
+#2.
+#Open and Read one of the Authors files into a dictionary.
+#The key to the dictionary will be the word the value will be the count
+#For example this sentence "A cat is a bat"
+#Will have a dictionary entry for A with the value of 2
+
+#
+#3 print out this dictionary
+#
