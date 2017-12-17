7a0cd54ea3e816b5bd4affaa0eba49aebb388ad0
Duncan Sollers <duncan.a.sollers@gmail.com>
2017-12-17 14:10:15 -0500

    Stop Words Working

--- WhichAuthor.py
+++ WhichAuthor.py
@@ -1,3 +1,11 @@
+#import nltk
+#nltk.download('punkt')
+#nltk.download('averaged_perceptron_tagger')
+
+import nltk.data
+from nltk.tokenize import word_tokenize
+import codecs
+import os
 
 
 print ("Hello World")
@@ -25,15 +33,48 @@ with open("smallwords/StopWords.txt") as f:
         stopwords[line2] = line2
 
 
-for k in stopwords.items():
-    print(k)
+#for k in stopwords.items():
+   # print(k)
 
 authorwords = {}
 with open("authors/dickens/ChristmasCarol/Charles-Dickens-Christmas-Carol") as f:
     for line in f:
         line2 = line.replace('\n','')
         authorwords[line2] = line2
+#V prints out dictionary V
+#for k in authorwords.items():
+#    print(k)
 
-for k in authorwords.items():
-    print(k)
+#doc = codecs.open('authors/dickens/ChristmasCarol/Charles-Dickens-Christmas-Carol', 'r', 'utf-8')
+doc = codecs.open('authors/dickens/OliverTwist/OliverTwistCharlesDickens.txt', 'r', 'utf-8')
+content = doc.read()
 
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+sentences = tokenizer.tokenize(content)
+
+wordcount = 0
+
+#
+#Step 3.
+#
+mesuredwords = {}
+for sentence in sentences:
+    #print ("#\n"+sentence+"\n#")
+    #words per Sentence
+    #most common words, but not small
+    wordlist = word_tokenize(sentence)
+    for word in wordlist:
+        word = word.lower()
+        if (not word in stopwords):
+            if (not word in mesuredwords):
+                mesuredwords[word] = 1
+            else:
+                mesuredwords[word] = mesuredwords[word] + 1
+            #print("\n"+word+"\n")
+
+
+#print dict sorted by key VVV
+for word in sorted(mesuredwords, key=mesuredwords.get, reverse=True):
+    if (mesuredwords[word] > 40):
+        print(word + "\t" + str(mesuredwords[word]))
